fe610b86ccff76a30fed2534d2cffa6ec78d5496
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-19 16:34:34 +0200

    Update checkIfInternet.java

--- internet/checkIfInternet.java
+++ internet/checkIfInternet.java
@@ -1,9 +1,9 @@
-			public void onClick(View v) {
-				ConnectivityManager cm2 = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-				NetworkInfo ni = cm2.getActiveNetworkInfo();
-				if(ni !=null && ni.isConnected()){
-					Toast.makeText(getApplicationContext(), "Internet ist da: ", Toast.LENGTH_SHORT).show();
-				}else {
-					Toast.makeText(getApplicationContext(), "Panic! Kein Internet!", Toast.LENGTH_SHORT).show();
-				}
-			}
+public void onClick(View v) {
+	ConnectivityManager cm2 = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+	NetworkInfo ni = cm2.getActiveNetworkInfo();
+	if(ni !=null && ni.isConnected()){
+		Toast.makeText(getApplicationContext(), "Internet ist da: ", Toast.LENGTH_SHORT).show();
+	}else {
+		Toast.makeText(getApplicationContext(), "Panic! Kein Internet!", Toast.LENGTH_SHORT).show();
+		}
+}
