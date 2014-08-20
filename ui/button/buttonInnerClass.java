778b6e1c5d32d8c097cab047cc62aca9f65b6998
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-20 11:32:19 +0200

    Delete buttonInnerClass.java

--- ui/button/buttonInnerClass.java
+++
@@ -1,9 +0,0 @@
-btn_blu_check = (Button)findViewById(R.id.btn_blu_check);
-        btn_blu_check.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(), "this is btn bluetooth check: " + h.checkForBluetooth(), Toast.LENGTH_SHORT).show();				
-			}
-		});
-    }
