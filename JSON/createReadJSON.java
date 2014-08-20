9af05d2751baadddbcdcc77565ea5bc4807e3c8d
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-20 13:05:21 +0200

    Create createReadJSON.java

---
+++ JSON/createReadJSON.java
@@ -0,0 +1,43 @@
+
+		
+		JSONObject jobj=new JSONObject();
+		JSONArray jarray = new JSONArray();
+		JSONObject jhash = new JSONObject();
+		try {
+			jobj.put("name", "GeoZilla");
+			jobj.put("array", jarray);
+			jarray.put(0, "zero");
+			jarray.put(1, "uno"); 
+			jobj.put("hash", jhash);
+			jhash.put("0", "0 zero");
+			jhash.put("1", "1 uno");
+			
+			/*
+			{
+				"hash": {
+					"1": "1 uno",
+					"0": "0 zero"
+				},
+				"array": ["zero",	"uno"],
+				"name": "GeoZilla"
+			}
+			*/
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(jobj);
+		try {
+			String name = jobj.getString("name").toString();
+			System.out.println(name);
+			String stray = jobj.getJSONArray("array").getString(0);
+			System.out.println(stray);
+			String strash = jobj.getJSONObject("hash").getString("1");
+			System.out.println(strash);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		startActivity(intent);
+	
