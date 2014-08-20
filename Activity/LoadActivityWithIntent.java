2ce746bb62b1b16610e8fa25fc549c4e2e34da87
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-20 13:06:54 +0200

    Update LoadActivityWithIntent.java

--- Activity/LoadActivityWithIntent.java
+++ Activity/LoadActivityWithIntent.java
@@ -10,10 +10,18 @@ b2 = (Button)findViewById(R.id.button2);
 		
 
 public void loadUIHelper(View v) {
-		Intent intent = new Intent(this, UILayoutHelper.class);
-		Person p = (Person) v.getTag(R.string.setTag_button2);
+	Intent intent = new Intent(this, UILayoutHelper.class);
+	Person p = (Person) v.getTag(R.string.setTag_button2);
 
-		Toast.makeText(getApplicationContext(), (String) v.getTag() + p.name,
-				Toast.LENGTH_LONG).show();
-		startActivity(intent);
-	}
+	Toast.makeText(getApplicationContext(), (String) v.getTag() + p.name,
+			Toast.LENGTH_LONG).show();
+	startActivity(intent);
+}
+
+public void sendMessage(View view){
+	Intent intent = new Intent(this,SomeText.class);
+	EditText editText = (EditText)findViewById(R.id.editText2);
+	String message = editText.getText().toString();
+	intent.putExtra(EXTRA_MESSAGE, message);
+	startActivity(intent);
+}
