02c55e8153577bc6e17ce0ad26c880a4c71952fb
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-20 14:52:52 +0200

    Update displayPopupWindow.java

--- ui/displayPopupWindow.java
+++ ui/displayPopupWindow.java
@@ -1,3 +1,30 @@
+//popup_content.xml in layout abspeichern
+//das Bild einfach ins drawable-hdpi reinziehen
+
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"
+    android:background="@drawable/popup_bg">
+    <TextView
+        android:id="@+id/tvCaption"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_centerHorizontal="true"
+        android:layout_centerVertical="true"
+        android:text="This is a popup222!"
+        android:textAppearance="?android:attr/textAppearanceLarge"
+        android:textColor="#ffffff" />
+</RelativeLayout>
+
+//aufrufen in
+btn_popup.setOnClickListener(new OnClickListener() {
+	
+	@Override
+	public void onClick(View v) {
+	displayPopupWindow(v);
+
+
 private void displayPopupWindow(View anchorView) {
 	    PopupWindow popup = new PopupWindow(Quizzer_MainActivity.this);
 	    View layout = getLayoutInflater().inflate(R.layout.popup_content, null);
