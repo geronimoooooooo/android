c2732787b376260b97652fe50c34fd815273888c
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-20 13:56:05 +0200

    Create dropDownMenu.java

---
+++ ui/dropDownMenu.java
@@ -0,0 +1,14 @@
+/popup_menu.xml in menu abspeichern
+
+<menu xmlns:android="http://schemas.android.com/apk/res/android">
+
+    <item  
+        android:id="@+id/menu1"  
+        android:icon="@drawable/ic_launcher"
+        android:title="@string/str_menu1"/>  
+
+    <item  
+        android:id="@+id/menu2"  
+        android:title="@string/str_menu2"/>  
+
+</menu>  
