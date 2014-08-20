14ba427f521d85c8ed1a0f4c33f6dd769d58c6e4
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-20 11:12:07 +0200

    Create checkIfBluetooth.java

---
+++ bluetooth/checkIfBluetooth.java
@@ -0,0 +1,19 @@
+	public final int REQUEST_ENABLE_BT= 1;
+	Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+public boolean checkForBluetooth(){
+		boolean bluetoothSupportedFromDevice= true;
+		
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			bluetoothSupportedFromDevice = false;
+			return bluetoothSupportedFromDevice;
+		}
+		
+		if(!mBluetoothAdapter.isEnabled()){
+			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//		    startActivityForResult(cameraIntent, REQUEST_ENABLE_BT);
+		}
+		
+		return bluetoothSupportedFromDevice;
+	}
