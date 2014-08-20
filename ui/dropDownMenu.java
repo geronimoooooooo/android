7594fa5e931ff6b26be580f7ab4d67a846dcfcea
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-20 14:18:54 +0200

    Update dropDownMenu.java

--- ui/dropDownMenu.java
+++ ui/dropDownMenu.java
@@ -10,3 +10,29 @@
         android:id="@+id/menu2"  
         android:title="@string/str_menu2"/>  
 </menu>  
+
+
+butPopup.setOnClickListener(new View.OnClickListener() {
+	
+	@Override
+	public void onClick(View v) {
+		
+		TableLayout table = (TableLayout)findViewById(R.id.tableLayout1);
+		table.setBackgroundColor(0xFF00FF00);
+		table.setBackgroundColor(getResources().getColor(R.color.blue));
+		
+		PopupMenu popup = new PopupMenu(Quizzer_MainActivity.this, butPopup);
+		popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+		Toast.makeText(getApplicationContext(), "popup", Toast.LENGTH_SHORT).show();
+		
+		popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+			
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				Toast.makeText(getApplicationContext(), "Clicked: "+item.getTitle(), Toast.LENGTH_SHORT).show();
+				return true;
+			}
+		});
+		popup.show();
+	}
+});
