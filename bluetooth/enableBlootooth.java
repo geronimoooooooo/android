301b2f7675a0f5b852ff7c55eb61d569c5b48763
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-21 10:34:56 +0200

    Update enableBlootooth.java

--- bluetooth/enableBlootooth.java
+++ bluetooth/enableBlootooth.java
@@ -1,13 +1,10 @@
- public void enableBlootooth(View view){
-    if (!myBluetoothAdapter.isEnabled()) {
-       Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-       startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
+public void enableBluetooth(View view) {
+	if (!myBluetoothAdapter.isEnabled()) {
+		Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
 
-       Toast.makeText(getApplicationContext(),"Bluetooth turned on" ,
-  		 Toast.LENGTH_LONG).show();
+		Toast.makeText(getApplicationContext(), "Bluetooth turned on",Toast.LENGTH_LONG).show();
+	} else {
+		Toast.makeText(getApplicationContext(), "Bluetooth is already on",Toast.LENGTH_LONG).show();
+	}
 }
-else{
-   Toast.makeText(getApplicationContext(),"Bluetooth is already on",
-      		 Toast.LENGTH_LONG).show();
-    }
- }
