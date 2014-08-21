2d35b131b470f171a1ff7e38f07930251d18959a
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-21 10:54:40 +0200

    Update startActivityForResult.java

--- Activity/startActivityForResult.java
+++ Activity/startActivityForResult.java
@@ -1,12 +1,12 @@
 public void on(View view) {
-		if (!myBluetoothAdapter.isEnabled()) {
-			Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
-			Toast.makeText(getApplicationContext(), "Bluetooth turned on",Toast.LENGTH_LONG).show();
-		} else {
-			Toast.makeText(getApplicationContext(), "Bluetooth is already on",Toast.LENGTH_LONG).show();
-		}
+	if (!myBluetoothAdapter.isEnabled()) {
+		Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
+		Toast.makeText(getApplicationContext(), "Bluetooth turned on",Toast.LENGTH_LONG).show();
+	} else {
+		Toast.makeText(getApplicationContext(), "Bluetooth is already on",Toast.LENGTH_LONG).show();
 	}
+}
    
    @Override
    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
