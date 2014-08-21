8c4fb70d9d5281ce8e6b89fcc5be9300ee39cbea
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-21 14:40:10 +0200

    Update BroadcastReceivers.java

--- Activity/BroadcastReceivers.java
+++ Activity/BroadcastReceivers.java
@@ -1,3 +1,15 @@
+//in onCreate aufrufen
+registerReceiver(bcrec, new IntentFilter("Alert"));
+
+//eigenen dynamischen broadcast receiver definieren
+final BroadcastReceiver bcrec = new BroadcastReceiver() {
+	public void onReceive(android.content.Context context,
+			android.content.Intent intent) {
+		String content = intent.getStringExtra("data");
+		Toast.makeText(context, content, Toast.LENGTH_SHORT).show();
+	}
+};
+
 public void startAlert(View view) {
 	Intent intent2 = new Intent("Alert");
 	intent2.putExtra("data", "valjupijojue");
@@ -14,4 +26,24 @@ public void startAlert(View view) {
 			+ (i * 1000), pendingIntent);
 	Toast.makeText(this, "Alarm set in " + i + " seconds",
 			Toast.LENGTH_LONG).show();
+}
+
+
+package com.example.broadcastertest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		Toast.makeText(context, "Don't panik but your time is up!!!!.",Toast.LENGTH_LONG).show();
+		Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+		vibrator.vibrate(2000);
+	}
 }
