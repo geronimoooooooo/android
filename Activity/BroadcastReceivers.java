3b12b22baf608c79c4581e3e36f13c9addf3b124
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-21 14:29:10 +0200

    Create BroadcastReceivers.java

---
+++ Activity/BroadcastReceivers.java
@@ -0,0 +1,10 @@
+final BroadcastReceiver bcrec = new BroadcastReceiver() {
+	public void onReceive(android.content.Context context,
+			android.content.Intent intent) {
+		String content = intent.getStringExtra("data");
+		Toast.makeText(context, content, Toast.LENGTH_SHORT).show();
+	}
+};
+
+//in onCreate() aufrufen
+registerReceiver(bcrec, new IntentFilter("Alert"));
