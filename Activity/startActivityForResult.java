bb4ea61051d4d901255f285d2614996a888bb288
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-21 10:53:32 +0200

    Update startActivityForResult.java

--- Activity/startActivityForResult.java
+++ Activity/startActivityForResult.java
@@ -1,20 +1,21 @@
-  public void on(View view) {
-	if (!myBluetoothAdapter.isEnabled()) {
-		Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-		startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
-		Toast.makeText(getApplicationContext(), "Bluetooth turned on",Toast.LENGTH_LONG).show();
-	} else {
-		Toast.makeText(getApplicationContext(), "Bluetooth is already on",Toast.LENGTH_LONG).show();
+public void on(View view) {
+		if (!myBluetoothAdapter.isEnabled()) {
+			Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
+			Toast.makeText(getApplicationContext(), "Bluetooth turned on",Toast.LENGTH_LONG).show();
+		} else {
+			Toast.makeText(getApplicationContext(), "Bluetooth is already on",Toast.LENGTH_LONG).show();
+		}
 	}
-}
- 
- @Override
- protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-   // TODO Auto-generated method stub
-   if(requestCode == REQUEST_ENABLE_BT){
-	   if(myBluetoothAdapter.isEnabled()) {
-		   text.setText("Status: Enabled");
-	   } else {   
-		   text.setText("Status: Disabled");
-	   }	   }
- }
+   
+   @Override
+   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	   // TODO Auto-generated method stub
+	   if(requestCode == REQUEST_ENABLE_BT){
+		   if(myBluetoothAdapter.isEnabled()) {
+			   text.setText("Status: Enabled");
+		   } else {   
+			   text.setText("Status: Disabled");
+		   }
+	   }
+   }
