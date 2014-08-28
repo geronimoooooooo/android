ea14a3e0e6509b6de9e078f966bb88e39b08ad0f
geronimoooooooo <geronimoooooooo@gmail.com>
2014-08-28 13:59:15 +0200

    Create ToastColour.java

---
+++ ui/ToastColour.java
@@ -0,0 +1,4 @@
+			Toast toast = Toast.makeText(this, "Bluetooth is active.", Toast.LENGTH_SHORT);
+			TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+			v.setTextColor(Color.GREEN);
+			toast.show();	
