d87627757d1554888e37e82b5c4811d4d1b74839
geronimoooooooo <geronimoooooooo@gmail.com>
2014-09-04 12:22:49 +0200

    Create strictmode.java

---
+++ internet/strictmode.java
@@ -0,0 +1,5 @@
+//ohne die 2 folgenden Zeilen würde es nicht gehen. Kann wohl nicht im gleichen Thread UI + Internetanfrage machen
+//wird gebraucht damit man in der app im gleichen Thread aufs Internet zugreifen kann!
+  
+   StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+   StrictMode.setThreadPolicy(policy);
