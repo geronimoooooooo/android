c10400f94064553a268a0f0513fa6ea696254360
geronimoooooooo <geronimoooooooo@gmail.com>
2014-10-31 14:23:16 +0100

    Create readTextFile.java

---
+++ io/readTextFile.java
@@ -0,0 +1,21 @@
+InputStream is = null;
+        try {
+            is = getResources().getAssets().open("text.txt");
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String text = sb.toString();
+        System.out.println("Text: "+text);
