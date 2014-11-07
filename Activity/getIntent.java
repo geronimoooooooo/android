e5b1bf5d0e6ea9cc753927d4e3d48e7680676422
geronimoooooooo <geronimoooooooo@gmail.com>
2014-11-07 15:44:41 +0100

    Create getIntent.java

---
+++ Activity/getIntent.java
@@ -0,0 +1,16 @@
+//nachdem die Activity aus einer anderen gestartet wurde
+public final static String EXTRA_MESSAGE ="my extra message!";
+
+public void sendMessage(View view){
+		Intent intent = new Intent(this,SomeText.class);
+		EditText editText = (EditText)findViewById(R.id.editText2);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+	
+      Intent intent = getIntent();
+	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+	    System.out.println(message);
+	    
+	    
