d601ba69702b53a8ea6716307f9fec4fddda66e2
geronimoooooooo <geronimoooooooo@gmail.com>
2014-11-25 11:29:10 +0100

    Update logging.java

--- logging.java
+++ logging.java
@@ -1,3 +1,6 @@
 Log.i("godzillaMethode", "iroar!");
 Log.i("DaySevenActivity", "This is an info message");
 Log.e("DaySevenActivity", "This is an error message");
+
+disable debugging in manifest.xml 
+removing the android:debuggable attribute or setting it to false
